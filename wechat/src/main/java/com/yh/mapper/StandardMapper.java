36be81bca2bde3ad6489d5570d834490e9d74cda
a1323377 <43374479+a1323377@users.noreply.github.com>
2019-10-08 08:16:15 +0800

    Update StandardMapper.java

--- wechat/src/main/java/com/yh/mapper/StandardMapper.java
+++ wechat/src/main/java/com/yh/mapper/StandardMapper.java
@@ -1,13 +1,10 @@
 package com.yh.mapper;
 
 import com.yh.domain.Standard;
-<<<<<<< HEAD
 
-=======
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
->>>>>>> 7c693f6cafa87bf71c7972c784d59491b780f54d
 public interface StandardMapper {
     int deleteByPrimaryKey(Integer id);
 
