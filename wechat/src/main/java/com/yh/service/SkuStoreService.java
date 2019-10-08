333717ab5b1c9673173934e24a8eabb98c991f15
a1323377 <43374479+a1323377@users.noreply.github.com>
2019-10-08 08:21:11 +0800

    Update SkuStoreService.java

--- wechat/src/main/java/com/yh/service/SkuStoreService.java
+++ wechat/src/main/java/com/yh/service/SkuStoreService.java
@@ -1,9 +1,8 @@
 package com.yh.service;
-
+import java.util.List;
+import java.util.Map;
 import com.yh.domain.SkuStore;
-<<<<<<< HEAD
 public interface SkuStoreService{
-
 
     int deleteByPrimaryKey(Integer id);
 
@@ -16,18 +15,16 @@ public interface SkuStoreService{
     int updateByPrimaryKeySelective(SkuStore record);
 
     int updateByPrimaryKey(SkuStore record);
-
-=======
-
-import java.util.List;
-import java.util.Map;
-
-public interface SkuStoreService {
+    
     List<Map<String,String>> selectSkuOrderByCreateTime();
+    
     List<Map<String,String>> selectSkuRush();
+    
     Map<String,String> selectSkuDetail(int skuId);
+    
     List<Map<String,String>> selectSkuScrollImg(int skuId);
+    
     List<Map<String,String>> selectSkuByProductId(int productId);
+    
     SkuStore selectSku(int skuId);
->>>>>>> 7c693f6cafa87bf71c7972c784d59491b780f54d
 }
